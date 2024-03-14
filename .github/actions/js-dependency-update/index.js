3c786b538cf2128859b8800389f076443fca9e9e
Mohamed Abdel Aziz <maziz.yahya00@gmail.com>
2024-03-14 18:31:56 +0400

    feat(17-custom-actions-JS): rename target-branch to head-branch + code refactoring

--- .github/actions/js-dependency-update/index.js
+++ .github/actions/js-dependency-update/index.js
@@ -7,17 +7,33 @@ const validateBranchName = ({ branchName }) =>
 const validateDirectoryName = ({ dirName }) =>
   /^[a-zA-Z0-9_\-\/]+$/.test(dirName);
 
+const setupLogger = ({ debug, perfix } = { debug: false, prefix: ''}) => ({
+    debug: (message) => {
+        if (debug) {
+            core.info(`DEBUG ${prefix}${prefix ? ' : ' : ''}${message}`);
+        }
+    },
+    info: (message) => {
+        core.info(`${prefix}${prefix ? ' : ' : ''}${message}`);
+    },
+    error: () => {
+        core.error(`${prefix}${prefix ? ' : ' : ''}${message}`);
+    }
+})
 async function run() {
   const baseBranch = core.getInput('base-branch', { required: true });
-  const targetBranch = core.getInput('target-branch', { required: true });
+  const headBranch = core.getInput('head-branch', { required: true });
   const ghToken = core.getInput('gh-token', { required: true });
   const workingDir = core.getInput('working-directory', { required: true });
   const debug = core.getBooleanInput('debug');
+  const logger = setupLogger({ debug, prefix: '[js-dependency-update]' });
 
   const commonExecOpts = {
     cwd: workingDir,
   };
   core.setSecret(ghToken);
+
+  logger.debug('Validating inputs - base-branch, head-branch, working-directory');
 
   if (!validateBranchName({ branchName: baseBranch })) {
     core.setFailed(
@@ -26,9 +42,9 @@ async function run() {
     return;
   }
 
-  if (!validateBranchName({ branchName: targetBranch })) {
+  if (!validateBranchName({ branchName: headBranch })) {
     core.setFailed(
-      'Invalid target-branch name. Branch names should include only characters, numbers, hyphens, underscores, dots, and forward slashes.'
+      'Invalid head-branch name. Branch names should include only characters, numbers, hyphens, underscores, dots, and forward slashes.'
     );
     return;
   }
@@ -40,9 +56,11 @@ async function run() {
     return;
   }
 
-  core.info(`[js-dependency-update] : base branch is ${baseBranch}`);
-  core.info(`[js-dependency-update] : target branch is ${targetBranch}`);
-  core.info(`[js-dependency-update] : working directory is ${workingDir}`);
+  logger.debug(`Base branch is ${baseBranch}`);
+  logger.debug(`Head branch is ${headBranch}`);
+  logger.debug(`Working directory is ${workingDir}`);
+
+  logger.debug('Checking for package updates')
 
   await exec.exec('npm update', [], {
     ...commonExecOpts,
@@ -57,10 +75,13 @@ async function run() {
   );
 
   if (gitStatus.stdout.length > 0) {
-    core.info('[js-dependency-update] : There are updates available!');
+    logger.debug('There are updates available!');
+    logger.debug('Setting up git');
     await exec.exec(`git config --global user.name "gh-automation"`);
     await exec.exec(`git config --global user.email "gh-automation@email.com"`);
-    await exec.exec(`git checkout -b ${targetBranch}`, [], {
+
+    logger.debug('Commiting and pushing package*.json changes')
+    await exec.exec(`git checkout -b ${headBranch}`, [], {
       ...commonExecOpts,
     });
     await exec.exec(`git add package.json package-lock.json`, [], {
@@ -69,28 +90,30 @@ async function run() {
     await exec.exec(`git commit -m "chore: update dependencies`, [], {
       ...commonExecOpts,
     });
-    await exec.exec(`git push -u origin ${targetBranch} --force`, [], {
+    await exec.exec(`git push -u origin ${headBranch} --force`, [], {
       ...commonExecOpts,
     });
-
+    logger.debug('Fetching ocktkit API')
     const octokit = github.getOctokit(ghToken);
 
     try {
+      logger.debug(`Creating PR using head branch ${headBranch}`);
+        
       await octokit.rest.pulls.create({
         owner: github.context.repo.owner,
         repo: github.context.repo.repo,
         title: `Update NPM dependencies`,
         body: `This pull request updates NPM packages`,
         base: baseBranch,
-        head: targetBranch
+        head: headBranch
       });
     } catch (e) {
-      core.error('[js-dependency-update] : Something went wrong while creating the PR. Check logs below.')
+      logger.error('Something went wrong while creating the PR. Check logs below.')
       core.setFailed(e.message);
-      core.error(e);
+      logger.error(e);
     }
   } else {
-    core.info('[js-dependency-update] : No updates at this point in time.');
+    logger.info('No updates at this point in time.');
   }
   /*
   [DONE] 1. Parse inputs:
